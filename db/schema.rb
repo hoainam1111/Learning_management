2a705a02d9404b805c01a4f46d02fe516198a4eb
hoainam1111 <66334691+hoainam1111@users.noreply.github.com>
2024-10-08 23:37:40 +0700

    add position to lesson

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_143632) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_08_162252) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,6 +71,9 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_143632) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "paid"
+    t.string "stripe_price_id"
+    t.text "premium_description"
   end
 
   create_table "lessons", force: :cascade do |t|
@@ -80,6 +83,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_143632) do
     t.integer "course_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "position"
     t.index ["course_id"], name: "index_lessons_on_course_id"
   end
 
@@ -91,6 +95,10 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_08_143632) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
